4a9f53ce4781aff36651feef72a959e3e1353ae9
Akash NP <akashnp1998@gmail.com>
2023-03-04 21:34:27 +0530

    feat: add remaning transaction columns, add 'create' buttton

--- pages/transactions.js
+++ pages/transactions.js
@@ -6,18 +6,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import { supabase } from '@/lib/supabaseClient';
 
 export default function BasicTable({transactions}) {
   return (
     <Paper sx={{maxWidth: 650, margin: 'auto'}} >
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Transactions
+                </Typography>
+                <Button color="inherit">Add New</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
         <TableContainer component={Paper}>
         <Table aria-label="simple table">
             <TableHead>
             <TableRow>
                 <TableCell>Payee/Item</TableCell>
-                <TableCell align="right">Amount</TableCell>
+                <TableCell>Amount</TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Account</TableCell>
+                <TableCell>labels</TableCell>
             </TableRow>
             </TableHead>
             <TableBody>
@@ -26,10 +46,13 @@ export default function BasicTable({transactions}) {
                 key={row.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                 >
-                <TableCell component="th" scope="row">
-                    {row.item}
-                </TableCell>
-                <TableCell align="right">{row.amount}</TableCell>
+                    <TableCell component="th" scope="row">
+                        {row.item}
+                    </TableCell>
+                    <TableCell>{row.amount}</TableCell>
+                    <TableCell>{row.category.name}</TableCell>
+                    <TableCell>{row.account.name}</TableCell>
+                    <TableCell>{row.label}</TableCell>
                 </TableRow>
             ))}
             </TableBody>
@@ -40,7 +63,16 @@ export default function BasicTable({transactions}) {
 }
 
 export async function getServerSideProps() {
-    const { data } = await supabase.from('transaction').select();
+    const { data } = await supabase
+        .from('transaction')
+        .select(`
+            item,
+            amount,
+            category ( name ),
+            account ( name ),
+            label,
+            notes
+        `);
 
     return {
         props: {
